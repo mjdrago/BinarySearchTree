698a1858fbd121b7cbd50a188e638cc5e1f74078
mjdrago <mario.j.drago@gmail.com>
2018-03-01 19:59:45 -0600

    Created node class with left and right Node options, as well as properties to view the data in the node and to view and set the left and right nodes.

---
+++ BinarySearchTreeProject/BinarySearchTreeProject/BinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class BinarySearchTree
+    {
+        private Node root;
+
+        public void Add(int value)
+        {
+            Node nodeToAdd = new Node(value);
+
+            if (root == null)
+            {
+                root = nodeToAdd;
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ BinarySearchTreeProject/BinarySearchTreeProject/Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class Node
+    {
+        private int value;
+        private Node right;
+        private Node left;
+
+        public Node(int data)
+        {
+            value = data;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+        public Node Right
+        {
+            get
+            {
+                return right;
+            }
+            set
+            {
+                if (right == null)
+                {
+                    right = value;
+                }
+            }
+        }
+        public Node Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                if (left == null)
+                {
+                    left = value;
+                }
+            }
+        }
+    }
+}
